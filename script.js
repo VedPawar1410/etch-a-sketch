71a60c88257ee380f28c604ecc81cf619cb7a62a
Ved Pawar <vedpawar1410@gmail.com>
2025-09-29 12:24:59 +0530

    added button to make new user input custom grids

--- script.js
+++ script.js
@@ -21,7 +21,25 @@ for (let elem of hoverGrid) {
     elem.addEventListener('mouseenter', () => {
         elem.style.backgroundColor = 'blue';
     })
-    /* elem.addEventListener('mouseleave', () => {
+    elem.addEventListener('mouseleave', () => {
         elem.style.backgroundColor = '';
-    }) */
+    })
 };
+
+
+// Button and re-generate grid
+const btn = document.createElement('button');
+btn.textContent="New Grid";
+
+btn.addEventListener("click",()=>{
+    //user prompt to user to enter a grid of one side < 100
+    let gridUserInput;
+    do{
+        gridUserInput = prompt("Enter the grid size you want < 100",'');
+    }
+    while(!(Number(gridUserInput)<=100) || typeof gridUserInput !== 'string');
+    createGrid(gridUserInput);
+});
+
+//Use prepend() to add the button as the first child of the body.
+document.body.prepend(btn);
